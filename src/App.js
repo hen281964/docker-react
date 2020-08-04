8d628f2c8ae6f9d723960a8d70ac0398a5aa6861
Henning <henning.evers@gmx.net>
2020-08-04 19:36:00 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Moin Moin
+          Learn MoinMoin
         </a>
       </header>
     </div>
